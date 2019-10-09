4a19d5c03368ead419873b94915876ec87158773
ashouy <joaopaulokad@gmail.com>
2019-10-09 15:13:28 -0300

    refactor

--- src/main/ExemploFuzzy.java
+++ src/main/ExemploFuzzy.java
@@ -5,12 +5,12 @@ import net.sourceforge.jFuzzyLogic.FIS;
 import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
 import net.sourceforge.jFuzzyLogic.rule.Variable;
 
-public class BiomassaTeste {
-    public BiomassaTeste() {
+public class ExemploFuzzy {
+    public ExemploFuzzy() {
     }
 
     public static void main(String[] args) {
-        FIS fis = FIS.load("src/resource/biomassa.fcl", true);
+        FIS fis = FIS.load("src/resource/model.fcl", true);
         if (fis == null) {
             System.err.println("Can't load file");
             System.exit(1);
@@ -21,8 +21,8 @@ public class BiomassaTeste {
         fis.setVariable("pessoal", 60);
 
         fis.evaluate();
-        Variable biomassa = fis.getFunctionBlock("biomassa").getVariable("risco");
-        JFuzzyChart.get().chart(biomassa, biomassa.getDefuzzifier(), true);
-        System.out.println(biomassa.getValue());
+        Variable model = fis.getFunctionBlock("model").getVariable("risco");
+        JFuzzyChart.get().chart(model, model.getDefuzzifier(), true);
+        System.out.println(model.getValue());
     }
 }
